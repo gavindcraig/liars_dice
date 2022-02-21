c304f5699011f3eeb8df20485769fe5cc0547779
Gavin D. Craig <67200393+gavindcraig@users.noreply.github.com>
2022-02-21 09:20:29 -0600

    Create Game class to manage play

--- __init__.py
+++ __init__.py
@@ -15,31 +15,43 @@ class Die:
 class Round:
     """A class representing a round"""
 
-    def __init__(self, dice, players, sides=6):
-        self.sides = sides
-        self.players = players
+    def __init__(self, game):
         # TODO: DIVIDE DICE AMONG PLAYERS
         # TODO: THROW EXCEPTION IF DICE DOES NOT DIVIDE AMONG PLAYERS
-        self.dice = []
-        for i in range(1, dice+1):
-            self.dice.append(Die(self.sides))
-        for d in self.dice:
+        self.game = game
+        for d in game.dice:
             d.roll()
 
     def results(self):
         r = {}
-        r[1] = sum(d.state == 1 for d in self.dice)
-        for s in range(2, self.sides+1):
-            r[s] = sum(d.state == s for d in self.dice) + r[1]
-        for s in range(2, self.sides+1):
+        r[1] = sum(d.state == 1 for d in self.game.dice)
+        for s in range(2, self.game.sides+1):
+            r[s] = sum(d.state == s for d in self.game.dice) + r[1]
+        for s in range(2, self.game.sides+1):
             print(f'{s}: {r[s]}')
 
 
+class Game:
+    """A class representing a game of Liar's Dice"""
+
+    def __init__(self, no_dice, players, sides=6):
+        self.sides = sides
+        self.players = players
+        self.dice = []
+        for i in range(1, no_dice+1):
+            self.dice.append(Die(self.sides))
+
+    def play(self):
+        for i in range(len(self.dice),1,-1):
+            r = Round(self)
+            r.results()
+            self.dice.pop()
+            print('-'*20)
+
+
 def main():
-    for i in range(20,1,-1):
-        r = Round(dice=i, players=4)
-        r.results()
-        print('-'*20)
+    Game(20, 4).play()
+
 
 if __name__ == "__main__":
     main()
